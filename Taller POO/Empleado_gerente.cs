f22a2d91f7cca9e8ad7a5ad77b64ec1c212cbb57
Wilmer Buestan <96480728+WilmerBuestan@users.noreply.github.com>
2023-11-22 12:36:51 -0500

    Add files via upload

---
+++ Taller POO/Empleado_gerente.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+// Definición de la interfaz
+public interface interfazMostrar
+{
+    void mostrarInfo();
+}
+
+// Clase base Empleado que implementa la interfazMostrar
+public class Empleado : interfazMostrar
+{
+    // Propiedades de la clase Empleado
+    public String Nombre { get; set; }
+    public double Salario { get; set; }
+
+    // Constructor de la clase Empleado
+    public Empleado(String nombre, double salario)
+    {
+        Nombre = nombre;
+        Salario = salario;
+    }
+
+    // Método para calcular el salario anual
+    public double salarioAnual()
+    {
+        return Salario * 12;
+    }
+
+    // Implementación del método de la interfazMostrar
+    public void mostrarInfo()
+    {
+        Console.WriteLine($"Nombre: {Nombre}");
+        Console.WriteLine($"Salario Mensual: {Salario}");
+        Console.WriteLine($"Salario Anual: {salarioAnual():C}");
+    }
+}
+
+// Clase derivada Gerente que hereda de Empleado e implementa interfazMostrar
+public class Gerente : Empleado, interfazMostrar
+{
+    // Propiedad adicional para el Departamento del Gerente
+    public String Departamento { set; get; }
+
+    // Constructor de la clase Gerente
+    public Gerente(String nombre, double salario, String departamento) : base(nombre, salario)
+    {
+        Departamento = departamento;
+    }
+
+    // Nuevo método para mostrar información del Gerente, llamando al método de la clase base
+    public new void mostrarInfo()
+    {
+        base.mostrarInfo(); // Llama al método de la clase base (Empleado)
+        Console.WriteLine($"Departamento: {Departamento}");
+    }
+}
+
+// Clase principal Ingreso
+class Ingreso
+{
+    static void Main()
+    {
+        // Solicitar información del empleado al usuario
+        Console.WriteLine($"Ingrese Nombre del Empleado:");
+        string NombreEmpleado = Console.ReadLine();
+        Console.WriteLine($"Salario Empleado:");
+        double SalarioEmpleado;
+        while (!double.TryParse(Console.ReadLine(), out SalarioEmpleado))
+        {
+            Console.WriteLine("Error, Ingrese el valor correcto para el salario del Empleado");
+        }
+
+        // Crear instancia de Empleado con los datos ingresados
+        Empleado empleado = new Empleado(NombreEmpleado, SalarioEmpleado);
+
+        // Solicitar información del gerente al usuario
+        Console.WriteLine("Ingrese el nombre del gerente:");
+        string NombreGerente = Console.ReadLine();
+        Console.WriteLine($"Salario Gerente:");
+        double SalarioGerente;
+        while (!double.TryParse(Console.ReadLine(), out SalarioGerente))
+        {
+            Console.WriteLine("Error, Ingrese el valor correcto para el salario del Gerente");
+        }
+
+        Console.WriteLine("Ingrese nombre del departamento del gerente");
+        string DepartamentoGerente = Console.ReadLine();
+
+        // Crear instancia de Gerente con los datos ingresados
+        Gerente gerente = new Gerente(NombreGerente, SalarioGerente, DepartamentoGerente);
+
+        // Mostrar información utilizando polimorfismo a través de la interfazMostrar
+        Console.WriteLine("\n Informacion del Empleado");
+        mostrarInfo(empleado);
+
+        Console.WriteLine("\n Informacion del Gerente");
+        mostrarInfo(gerente);
+    }
+
+    // Método para mostrar información utilizando polimorfismo
+    static void mostrarInfo(interfazMostrar entidad)
+    {
+        entidad.mostrarInfo();
+    }
+}
---
+++ Taller POO/README.txt
@@ -0,0 +1,34 @@
+Instrucciones para ejecutar y probar la Aplicación de Gestión de Empleados
+Esta aplicación de gestión de empleados está desarrollada en C# y utiliza conceptos de Programación Orientada a Objetos (POO). Permite gestionar información sobre empleados y gerentes, calculando y mostrando detalles como el salario mensual, salario anual y el departamento (en caso de gerentes).
+
+Requisitos Previos
+Asegúrate de tener instalado:
+
+.NET SDK
+Pasos para Ejecutar la Aplicación
+Clonar el Repositorio:
+
+bash
+Copy code
+git clone https://github.com/WilmerBuestan/webAVZ/Taller Poo/
+Navegar al Directorio del Proyecto:
+
+bash
+Copy code
+cd nombre-del-repositorio
+Compilar la Aplicación:
+
+bash
+Copy code
+dotnet build
+Ejecutar la Aplicación:
+
+bash
+Copy code
+dotnet run
+Cómo Probar la Aplicación
+La aplicación solicitará información sobre un empleado y un gerente. Sigue las instrucciones en la consola para ingresar los datos requeridos.
+
+La aplicación mostrará la información ingresada y calculará el salario anual.
+
+Repite el proceso para probar con diferentes empleados y gerentes.
